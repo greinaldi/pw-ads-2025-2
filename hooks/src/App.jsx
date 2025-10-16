cddeaa82c020e302cf5f3bbd9564103b0fe42b92
greinaldi <gabrielreinaldi0@gmail.com>
2025-10-16 10:36:43 -0300

    (16/10) React Hooks: exercícios 04 e 05

--- hooks/src/App.jsx
+++ hooks/src/App.jsx
@@ -8,6 +8,7 @@ import Exercicio01 from './exercicios/01.jsx'
 import Exercicio02 from './exercicios/02.jsx'
 import Exercicio03 from './exercicios/03.jsx'
 import Exercicio04 from './exercicios/04.jsx'
+import Exercicio05 from './exercicios/05.jsx'
 
 function App() {
 
@@ -21,8 +22,8 @@ function App() {
           <li> <Link to="/02">Exercício 02</Link> </li>
           <li> <Link to="/03">Exercício 03</Link> </li>
           <li> <Link to="/04">Exercício 04</Link> </li>
+          <li> <Link to="/05">Exercício 05</Link> </li>
         </ul>
-        
         <hr />
 
         <Routes>
@@ -32,6 +33,7 @@ function App() {
           <Route path="/02" element={<Exercicio02 />} />
           <Route path="/03" element={<Exercicio03 />} />
           <Route path="/04" element={<Exercicio04 />} />
+          <Route path="/05" element={<Exercicio05 />} />
         </Routes>
       </BrowserRouter>
     </>
