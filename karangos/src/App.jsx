abd6b15c95d6f69529cfa3ddae2e359bda5011b8
greinaldi <gabrielreinaldi0@gmail.com>
2025-11-06 09:57:09 -0300

    (31/10) Tema, menu principal e barra de rodapé

--- karangos/src/App.jsx
+++ karangos/src/App.jsx
@@ -6,13 +6,26 @@ import HeaderBar from './ui/HeaderBar'
 import CssBaseline from '@mui/material/CssBaseline'
 
 
+import { ThemeProvider } from '@mui/material/styles'
+import theme from './ui/theme'
+
+
+import FooterBar from './ui/FooterBar'
+
+
 function App() {
  return (
    <>
-     <CssBaseline />
-     <BrowserRouter>
-       <HeaderBar />
-     </BrowserRouter>
+     <ThemeProvider theme={theme}>
+      
+       <CssBaseline />
+      
+       <BrowserRouter>
+         <HeaderBar />
+         <FooterBar />
+       </BrowserRouter>
+    
+     </ThemeProvider>
    </>
  )
 }
--- karangos/src/ui/HeaderBar.jsx
+++ karangos/src/ui/HeaderBar.jsx
@@ -2,19 +2,19 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import MainMenu from './MainMenu'
 
 import logotipo from '../assets/karangos-logo-600px.png'
 
 export default function HeaderBar() {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+      <AppBar position="static" enableColorOnDark>
         <Toolbar variant="dense">
           <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-            <MenuIcon />
-          </IconButton>
+           <MenuIcon />
+         </IconButton>
+          <MainMenu />
           <a href="/">
            <img src={logotipo} alt="Logotipo Karangos" title="Karangos &bull; Os velhinhos são nossa paixão" style={{ width: '300px' }} />
          </a>
